ecc023184e347d0e536bf5042831d95f5c4c9670
boukataya <youness.bouka@gmail.com>
2022-12-28 08:45:35 +0100

    Initial commit

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test00SanityCheck.java
@@ -0,0 +1,12 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.Test;
+
+class Test00SanityCheck {
+
+    @Test
+    void test() {
+        System.out.println("Should pass!!");
+    }
+
+}
